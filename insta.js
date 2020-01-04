88eef68f16bbe5197e8beb331e85eaac43b1ea5e
ravipatel2293 <ravipatel2293@gmail.com>
2020-01-04 15:39:12 +0530

    Update insta.js

--- insta.js
+++ insta.js
@@ -1,1 +1,7 @@
-<script src="//instant.page/3.0.0" type="module" defer integrity="sha384-OeDn4XE77tdHo8pGtE1apMPmAipjoxUQ++eeJa6EtJCfHlvijigWiJpD7VDPWXV1"></script>
+ var s = document.createElement("script");
+    s.type = "module";
+    s.setAttribute("defer","")
+    s["integrity"]="sha384-OeDn4XE77tdHo8pGtE1apMPmAipjoxUQ++eeJa6EtJCfHlvijigWiJpD7VDPWXV1"
+    s.src = "http://instant.page/3.0.0";
+  
+    document.head.appendChild(s);
